9140d0b01c0d87908839aa2acb98160d604afca4
Faiz Rasool <faizhashmi459@gmail.com>
2024-06-13 14:59:25 +0500

    Code update to DRY principle

--- script.js
+++ script.js
@@ -14,17 +14,25 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+//Display Message
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // When there is no input or 0 input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number!';
+    // document.querySelector('.message').textContent = 'No Number!';
+    displayMessage('No Number!');
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    // document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number');
+
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
@@ -32,43 +40,62 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
 
     //High Score function
-    if(score > highscore){
+    if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-    // When guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too High';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
+  }
 
-    // When guess is too low
-  } else if (guess < secretNumber) {
+  //When guess is wrong
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too Low';
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? 'Too High' : 'Too Low';
+      displayMessage(guess > secretNumber ? 'Too High' : 'Too Low');
+
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      // document.querySelector('.message').textContent = 'You lost the game!';
+      displayMessage('You lost the game!');
+
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  //   // When guess is too high
+  // } else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too High';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+
+  //   // When guess is too low
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too Low';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  score = 20; 
+  score = 20;
   // secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  
-  document.querySelector('.message').textContent = 'Start Guessing...';
+  // document.querySelector('.message').textContent = 'Start Guessing...';
+  displayMessage('Start Guessing...');
 
   document.querySelector('.score').textContent = score;
-  
+
   document.querySelector('.number').textContent = '?';
 
   document.querySelector('.guess').value = '';
